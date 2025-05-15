c776535489d0c921ea172eb9c9bb100fef14dd9d
Mike Solomon <mike.solomon@coinbase.com>
2025-05-15 11:46:38 -0700

    go to mainnet

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,9 +1,9 @@
 import { createThirdwebClient } from "thirdweb";
-import { baseSepolia } from "viem/chains";
+import { base } from "viem/chains";
 
-export const FINGER_ON_BUTTON_CONTRACT_ADDRESS = "0xc1e17f4259006c299e9cf8ae790eef1bd95b0618";
+export const FINGER_ON_BUTTON_CONTRACT_ADDRESS = "0x0b0a47a8f3fD6c5DF1ddE97f6427dFF487633A09";
 export const ENTRY_FEE = 0.00001;
-export const CHAIN = baseSepolia;
+export const CHAIN = base;
 export const THIRDWEB_BACKEND_WALLET = "0xb503723beC0E8142aC24aCf55Fc11c7fC809e723"
 
 export const client = createThirdwebClient({
